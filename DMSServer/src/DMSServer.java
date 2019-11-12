13d44f7e588a6fe0584b81ee9d20c129873c6f12
dldhk97 <dldhk97@gmail.com>
2019-11-12 17:04:42 +0900

    DMSServer 지명근 출첵

--- DMSServer/src/DMSServer.java
+++ DMSServer/src/DMSServer.java
@@ -4,5 +4,6 @@ public class DMSServer
 	public static void main(String[] args)
 	{
 		System.out.println("Dormitory Management System Server.");
+		System.out.println("20161185 지명근 git연동 성공");
 	}
 }
