2896aab758b99dd2ea6391ca6667cd2236598fbb
dldhk97 <dldhk97@gmail.com>
2019-11-12 18:49:45 +0900

    출석체크 메세지 삭제

--- DMSServer/src/DMSServer.java
+++ DMSServer/src/DMSServer.java
@@ -4,8 +4,5 @@ public class DMSServer
 	public static void main(String[] args)
 	{
 		System.out.println("Dormitory Management System Server.");
-		System.out.println("20161185 지명근 git연동 성공");
-		System.out.println("안녕하세요 박동현입니다.");
-		System.out.println("안녕하세요 지각생 정서희입니다.");
 	}
 }
