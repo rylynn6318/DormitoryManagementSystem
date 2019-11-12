613574b63873b42ad86eefa84b112ee7816fc2a7
LireEruel <jsh001505@gmail.com>
2019-11-12 17:33:53 +0900

    정서희 출석을 추가하였습니다.

--- DMSServer/src/DMSServer.java
+++ DMSServer/src/DMSServer.java
@@ -6,5 +6,6 @@ public class DMSServer
 		System.out.println("Dormitory Management System Server.");
 		System.out.println("20161185 지명근 git연동 성공");
 		System.out.println("안녕하세요 박동현입니다.");
+		System.out.println("안녕하세요 지각생 정서희입니다.");
 	}
 }
