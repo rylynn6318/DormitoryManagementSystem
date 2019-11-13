6ef1f0d3730d01fa6f4ef7fdb9a7d66248afacf7
hmlim <hmlim@192.168.208.123>
2019-11-13 09:34:46 +0900

    검사

--- DMSServer/src/DMSServer.java
+++ DMSServer/src/DMSServer.java
@@ -4,6 +4,6 @@ public class DMSServer
 	public static void main(String[] args)
 	{
 		System.out.println("Dormitory Management System Server.");
-		System.out.println("노트북 검사.");
+		System.out.println("노트북 검사");
 	}
 }
