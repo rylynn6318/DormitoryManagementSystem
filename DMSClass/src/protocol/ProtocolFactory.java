ab1a56a69e4073f89ed04ccb206dedd547fac619
lunaoeu-arch <lunaoeu@gmail.com>
2019-11-20 15:51:24 +0900

    프로토콜 제작 시작

---
+++ DMSClass/src/protocol/ProtocolFactory.java
@@ -0,0 +1,8 @@
+package protocol;
+
+public class ProtocolFactory{
+    public static IProtocol createProtocol(byte[] input){
+        switch(input[0]{
+        })
+    }
+}
---
+++ DMSClass/src/protocol/client/LoginProtocol.java
@@ -0,0 +1,18 @@
+package protocol.client;
+import protocol.IProtocol;
+
+// LoginProtocol
+//  클라이언트가 서버의 로그인을 할때 쓰는 프로토콜.
+// ┌ LOGIN_RESPONSE_ID, PW ┐ 시작 인덱스
+// ├───────┬───────────────┤  0
+// │   0   │ ProtocolType  │  
+// ├───────┼───────────────┤  LEN_PROTOCOL_TYPE
+// │ 1~21  │ Login Id      │
+// ├───────┼───────────────┤  LEN_LOGIN_ID
+// │ 1~21  │ Login PW      │
+// └───────┴───────────────┘ 최종 길이
+//							  LEN_PROTOCOL_TYPE + LEN_LOGIN_ID + LEN_LOGIN_PASSWORD
+
+public class LoginProtocol implements IProtocol{
+
+}
