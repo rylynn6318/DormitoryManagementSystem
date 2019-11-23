4ff5548605189164198825763cc4ab8e62a5fef8
dldhk97 <dldhk97@gmail.com>
2019-11-23 17:36:36 +0900

    DMS Client Fx 메인 페이지 탭 수정

--- DMSClientFx/src/controller/MainPageController.java
+++ DMSClientFx/src/controller/MainPageController.java
@@ -13,6 +13,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
@@ -24,7 +25,7 @@ public class MainPageController implements Initializable
 {
 
     @FXML
-    private ListView<String> NavigationListView;
+    private ListView<NavigationTab> NavigationListView;
     
     @FXML
     private TabPane MainTabPane;
@@ -44,15 +45,36 @@ public class MainPageController implements Initializable
 	private void initializeNavigationTabs()
 	{
 		//디버깅용 네비게이션 탭 객체 설정
-		NavigationListView.setItems(FXCollections.observableArrayList());
+		//NavigationListView.setItems(FXCollections.observableArrayList());			//가장 기본적은 설정법인데, NavigationListView<String>밖에 안됨.
 		
-		//디버깅용 네비게이션 탭 객체 추가
-		NavigationListView.getItems().add("생활관 입사 신청");
-		NavigationListView.getItems().add("생활관 호실 확인");
-		NavigationListView.getItems().add("생활관 신청확인");
-		NavigationListView.getItems().add("생활관 고지서 출력");
-		NavigationListView.getItems().add("결핵진단서 제출");
-		NavigationListView.getItems().add("결핵진단서 조회");
+		NavigationListView.setCellFactory(param -> new ListCell<NavigationTab>() 
+		{
+		    @Override
+		    protected void updateItem(NavigationTab item, boolean empty) {
+		        super.updateItem(item, empty);
+
+		        if (empty || item == null || item.getTitle() == null) 
+		        {
+		            setText(null);
+		        } 
+		        else 
+		        {
+		            setText(item.getTitle());
+		        }
+		    }
+		});
+		
+		//테스트용 네비게이션 탭 객체 추가
+		NavigationTab tab1 = new NavigationTab("생활관 입사 신청", TabType.SubmitApplication);
+		NavigationTab tab2 = new NavigationTab("생활관 호실 확인", TabType.CheckApplication);
+		NavigationTab tab3 = new NavigationTab("생활관 신청확인", TabType.SubmitApplication);
+		NavigationTab tab4 = new NavigationTab("생활관 고지서 출력", TabType.SubmitApplication);
+		NavigationTab tab5 = new NavigationTab("결핵진단서 제출", TabType.CheckApplication);
+		NavigationTab tab6 = new NavigationTab("결핵진단서 조회", TabType.SubmitApplication);
+		
+		NavigationListView.getItems().add(tab1);
+		NavigationListView.getItems().add(tab2);
+		NavigationListView.getItems().add(tab3);
 		
 		//리스트뷰 더블클릭 이벤트 처리. 메뉴타입이 클래스로 바뀌면 String을 클래스명으로 바꾸셈.
 		NavigationListView.setOnMouseClicked(new EventHandler<MouseEvent>() 
@@ -63,39 +85,30 @@ public class MainPageController implements Initializable
 		        if (click.getClickCount() == 2) 
 		        {
 		        	//이번에 선택된 아이템을 찾는다.
-		        	String currentItemSelected = NavigationListView.getSelectionModel().getSelectedItem();
-		        	
-		        	// Main Tab Pane에 추가할 Tab 생성
-		        	Tab curTab = new Tab(currentItemSelected);
+		        	NavigationTab currentItemSelected = NavigationListView.getSelectionModel().getSelectedItem();
 		        	
 		        	try
 					{
 						VBox a = FXMLLoader.load(getClass().getResource("/page/CheckApplicationTab.fxml"));
-						curTab.setContent(a);
+						currentItemSelected.setContent(a);
 					} 
 		        	catch (IOException e)
 					{
 		        		System.out.println(e.getMessage());
 					}
-		        	MainTabPane.getTabs().add(curTab);
-		        	NavigationListView.getSelectionModel().select(curTab.getText());
+		        	MainTabPane.getTabs().add(currentItemSelected);
+		        	NavigationListView.getSelectionModel().select(currentItemSelected);
+		        	
+		        	//테스트
+		        	Tab a = new Tab("Aa");
+		        	MainTabPane.getTabs().add(a);
 		        }
 		    }
 		});
 	}
 }
 
-class NavigationBarClickEventHanlder implements EventHandler<MouseEvent>
-{
-	@Override
-	public void handle(MouseEvent event)
-	{
-		// TODO 네비게이션 바에서 이벤트 핸들러를 별개로 구성할거면 여기에다 코드를 넣어라.
-		
-	}
-}
-
-class NavigationTab
+class NavigationTab extends Tab
 {
 	private String title;
 	private TabType tabtype;
@@ -105,6 +118,23 @@ class NavigationTab
 		this.title = title;
 		this.tabtype = tabType;
 	}
+
+	public String getTitle()
+	{
+		return title;
+	}
+
+	public void setTitle(String title)
+	{
+		this.title = title;
+	}
+
+	public TabType getTabtype()
+	{
+		return tabtype;
+	}
+
+	
 }
 
 enum TabType
@@ -115,5 +145,15 @@ enum TabType
 	private TabType(int value)
 	{
 		this.value = value;
+	}
+}
+
+class NavigationBarClickEventHanlder implements EventHandler<MouseEvent>
+{
+	@Override
+	public void handle(MouseEvent event)
+	{
+		// TODO 네비게이션 바에서 이벤트 핸들러를 별개로 구성할거면 여기에다 코드를 넣어라.
+		
 	}
 }
