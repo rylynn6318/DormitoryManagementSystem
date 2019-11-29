df238aa2557477bf10bad0025bd2352528da608d
lunaoeu-arch <lunaoeu@gmail.com>
2019-11-29 10:46:30 +0900

    프르토콜 뼈대 만드는중

--- DMSClass/src/protocol/AbstractProtocol.java
+++ DMSClass/src/protocol/AbstractProtocol.java
@@ -1,24 +1,51 @@
 package protocol;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public abstract class AbstractProtocol {
     private ProtocolHeader head;
-    private byte[] packet;
+    private byte[] body;
 
-    protected AbstractProtocol(byte[] packet) {
-        this.packet = packet;
+    // 새 프로토콜 만들고 head 할당함. body는 setBody로 따로 할당하셈.
+    protected AbstractProtocol(ProtocolHeader head) {
+        this.head = head;
     }
 
-    public void makeProtocol(Serializable obj) throws IOException {
+    // 패킷으로부터 프로토콜을 만듬.
+    public static AbstractProtocol createProtocol(byte[] packet) {
+        return null;
+    }
+
+    // head를 바이트 배열로 바꿔서 이를 body랑 합쳐 반환함.
+    public byte[] getBytes() {
+        byte[] headbyte = head.getBytes();
+        byte[] packet = new byte[headbyte.length + body.length];
+        System.arraycopy(headbyte, 0, packet, 0, headbyte.length);
+        System.arraycopy(body, 0, packet, headbyte.length, body.length);
+        return packet;
+    }
+
+    // Serializable 객체를 직렬화 해서 body에 넣음.
+    public void setBody(Serializable obj) throws IOException {
         try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
             try (ObjectOutputStream oos = new ObjectOutputStream(baos)) {
                 oos.writeObject(obj);
-                // serializedMember -> 직렬화된 member 객체 
-                packet = baos.toByteArray();
+                body = baos.toByteArray();
+            }
+        }
+    }
+
+    // body에 있는 바이트 배열을 역직렬화해서 Serializable 객체 반환. 이후 캐스팅은 알아서 하셈.
+    public Serializable getBody() throws ClassNotFoundException, IOException {
+        try (ByteArrayInputStream bais = new ByteArrayInputStream(body)) {
+            try (ObjectInputStream ois = new ObjectInputStream(bais)) {
+                Object obj = ois.readObject();
+                return (Serializable) obj;
             }
         }
     }
--- DMSClass/src/protocol/ProtocolHeader.java
+++ DMSClass/src/protocol/ProtocolHeader.java
@@ -1,11 +1,19 @@
 package protocol;
 
-class ProtocolHeader{
-    public static final int SIZE_TYPE = 8;
-    
-    private byte[] type = new byte[SIZE_TYPE];
+import java.io.ByteArrayOutputStream;
 
-    public ProtocolType type(){
-        return ProtocolType.getType();
+public class ProtocolHeader {
+    private short length;
+    private byte type;
+    private byte response_direction;
+    private byte code;
+
+    public byte[] getBytes() {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        baos.write(length);
+        baos.write(type);
+        baos.write(response_direction);
+        baos.write(code);
+        return baos.toByteArray();
     }
 }
