545472a1f2a88f1c0962b5d808df963996fd487a
sm <lunaoeu@gmail.com>
2019-11-29 18:16:26 +0900

    프로토콜 추가

---
+++ DMSClass/src/protocol/EventProtocol.java
@@ -0,0 +1,10 @@
+package protocol;
+
+public class EventProtocol extends AbstractProtocol{
+
+    protected EventProtocol(ProtocolHeader head) {
+        super(head);
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ DMSClass/src/protocol/FileProtocol.java
@@ -0,0 +1,10 @@
+package protocol;
+
+public class FileProtocol extends AbstractProtocol{
+
+    protected FileProtocol(ProtocolHeader head) {
+        super(head);
+        // TODO Auto-generated constructor stub
+    }
+    
+}
