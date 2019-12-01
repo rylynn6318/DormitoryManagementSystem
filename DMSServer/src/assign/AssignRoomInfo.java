ab871120eef6ab5ac872ebc8b54164ba68e4ea04
stimfirebat <57317429+stimfirebat@users.noreply.github.com>
2019-12-01 16:00:07 +0900

    Update AssignRoomInfo.java

--- DMSServer/src/assign/AssignRoomInfo.java
+++ DMSServer/src/assign/AssignRoomInfo.java
@@ -1,7 +1,7 @@
 package assign;
 
 import shared.classes.RoomInfo;
-//RoomInfo를 상속받아서 배정에 필요한 정보를 더 넣은 
+//RoomInfo를 상속받아서 배정에 필요한 정보를 더 넣은 클래스
 public class AssignRoomInfo extends RoomInfo  {
 	private String seat;
 	private boolean isEmpty;
