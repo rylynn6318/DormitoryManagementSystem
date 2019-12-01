95d11e440b68a049c5ad4612c8bfca79adcdf372
sm <lunaoeu@gmail.com>
2019-12-01 16:55:15 +0900

    프로토콜 통폐합

--- DMSClass/src/protocol/Protocol.java
+++ DMSClass/src/protocol/Protocol.java
@@ -47,29 +47,13 @@ import java.util.Arrays;
 // }
 // 대충 이런 느낌으로? 처리해야 할듯?? 아직 구현 안해서 확답 못함ㅎ
 
-// 사실상 헤더만 바뀌고 프로토콜은 다 똑같은데 왜 여러개 상속해서 쓰느냐?
-// 1. File 같은 경우 분할해야 하는데 File 클래스만 넘겨주면 알아서 분할해서 보내고 받게 할려고
-// 2. 헤더에 타입 등의 정보를 자동으로 넣게 할려고
-// 생각해보니 굳이 안 나누고 헤더만 분리해서 써도 될것 같기도 하고;;;;
-// 근데 분할되서 온걸 하나의 프로토콜로 합쳐서 처리할려면 AbstractSplittableProtocol 만들어서 거기도 또 처리하던가 해야할듯;;;;
-// 않의 근데 소켓 하나로 통신할껀데 이러면 두번째로 보내는거부턴 헤더 빼고 보내도 되는거 아닌가?
-// 어짜피 리턴 받을때까지 클라이언트 블락 걸어서 데이터 또 못보내게 하면 다른 프로토콜 오는것도 없을텐데
-// 애초에 요청 하나에 소켓 하나라서 진짜 헤더 더 안보내도 될듯; 월요일에 선명킴한테 물어봐야지
-// 만약 그렇다면 AbstractSplittableHeader를 쓸 필요도 없을듯
-
-// 위에 의식의 흐름대로 주석 쓰고 든 생각 정리
-// 1. stateless 서버라면 한번에 하나의 프로토콜만 전송된다. 소켓 레벨에서 다른 프로토콜이랑 썪일 이유가 없다. 그렇다면 분할되는 프로토콜에 헤더 넣어줄 필요가 있나?
-//    즉, [header|body] + [header|body] + ... 이렇게 말고 [header|body] + [body] + [body] 이렇게 보내도 되는거 아닌가? 내가 잘못 알고 있나?
-// 2. 프로토콜을 받는건 그렇다 치고, 보낼때 Protocol 타입을 명시 해야할까? Header만 만들어서 넣어주면 body엔 머가 드가든 상관 없지 않나?
-//    일단 Header는 확실히 type에 따라 클래스를 나눠야한다. 여기서 Protocol까지 나눠야 할 이유가 있을까
-
-public abstract class AbstractProtocol {
+public class Protocol {
     private AbstractHeader header;
     private byte[] body;
 
     // 새 프로토콜 만들고 head 할당함.
     // 일단은 protected로 만듬 2019.12.01 05:31
-    protected AbstractProtocol(byte[] packet) {
+    protected Protocol(byte[] packet) {
         this.header = AbstractHeader.create(packet);
         this.body = Arrays.copyOfRange(packet, header.getHeaderLength(), packet.length);
     }
