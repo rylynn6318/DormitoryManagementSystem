83d2e189c41f012cb731a234c34a191a7a709999
park dong hyeon <park dong hyeon@DESKTOP-D7AFTRF>
2019-12-05 00:23:37 +0900

    거리가산점 계산기 만듬

---
+++ DMSServer/src/DistanceScore.java
@@ -0,0 +1,18 @@
+
+public class DistanceScore {
+	public static double getDistanceScore(String s)
+	{
+		double b;
+		int a = Integer.parseInt(s);
+		int c = Integer.parseInt(s);
+		if(c/100 == 402) return 0.4;	//울릉도
+		
+		a = a/1000;
+		if(a==63) return 0.4;	//제주도
+		else if(35 <a && a<44) return 0.1;	//경북, 대구
+		else if(43 <a && a<54) return 0.2;	//울산, 부산, 경남
+		else if(33 <a && a<36) return 0.2;	//대전
+		else
+			return 0.3;
+	}
+}
