46680a645a12c3b95d9913473dfa7b7b31b9f920
sm <lunaoeu@gmail.com>
2019-12-07 08:21:18 +0900

    클라, 서버 두곳에서 쓰는 소켓 헬퍼 도입

---
+++ DMSClass/src/ultils/SocketHelper.java
@@ -0,0 +1,35 @@
+package ultils;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+// 프로토콜을 이용한 소켓 통신을 쉽게 해주는 wrapper 클래스
+public class SocketHelper {
+    public final static String host = "127.0.0.1";
+    public final static int port = 4444;
+    public final static int sendbuffer_size = 1024;
+
+    private Socket socket = null;
+
+    SocketHelper(Socket socket){
+        this.socket = socket;
+    }
+
+    public void write(Protocol p) throws IOException {
+        for(Protocol protocol : ProtocolHelper.split(p, sendbuffer_size)){
+            socket.getOutputStream().write(protocol.getPacket());
+        }
+    }
+
+    public Protocol read() throws Exception {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        byte[] buffer = new byte[sendbuffer_size];
+
+        while (socket.getInputStream().read(buffer) != -1) {
+            output.write(buffer);
+        }
+
+        return new Protocol.Builder(output.toByteArray()).build();
+    }
+}
