3fa0ac132fad5564985fb59ff863e75fc8ffc8c0
park dong hyeon <park dong hyeon@DESKTOP-D7AFTRF>
2019-12-07 01:31:44 +0900

    dd

---
+++ DMSServer/src/DBinfo.java
@@ -0,0 +1,20 @@
+
+
+
+public class DBinfo {
+	static final String DRIVER_NAME = "mysql";
+	static final String HOSTNAME = "wehatejava.czztgstzacsv.us-east-1.rds.amazonaws.com";
+	static final String PORT = "3306";
+	static final String DB_NAME = "Prototype";													//DB이름
+	static final String USER_NAME = "admin"; 													//DB에 접속할 사용자 이름을 상수로 정의
+	static final String PASSWORD = "En2i3oHKLGh9UlnbYFP1"; 										//사용자의 비밀번호를 상수로 정의
+	static final String DB_URL = 
+					"jdbc:" + 
+					DRIVER_NAME + "://" + 
+					HOSTNAME + ":" + 
+					PORT + "/" + 
+					DB_NAME + "?user=" + 
+					USER_NAME + "&password=" + 
+					PASSWORD; 
+	
+}
