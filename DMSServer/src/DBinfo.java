eec2910f2eb7fabb9f46374da7ce49a6cf8d894d
park dong hyeon <park dong hyeon@DESKTOP-D7AFTRF>
2019-12-07 01:22:23 +0900

    DBinfo enum으로 변경

--- DMSServer/src/DBinfo.java
+++ DMSServer/src/DBinfo.java
@@ -1,41 +1,22 @@
 
 
-public class DBinfo {
-	static final String DRIVER_NAME = "mysql";
-	static final String HOSTNAME = "wehatejava.czztgstzacsv.us-east-1.rds.amazonaws.com";
-	static final String PORT = "3306";
-	static final String DB_NAME = "Prototype";													//DB이름
-	static final String USER_NAME = "admin"; 													//DB에 접속할 사용자 이름을 상수로 정의
-	static final String PASSWORD = "En2i3oHKLGh9UlnbYFP1"; 										//사용자의 비밀번호를 상수로 정의
-	static final String DB_URL = 
-					"jdbc:" + 
+public enum DBinfo {
+	DRIVER_NAME("mysql"),
+	HOSTNAME("wehatejava.czztgstzacsv.us-east-1.rds.amazonaws.com"),
+	PORT("3306"),
+	DB_NAME("Prototype"),											//DB이름
+	USER_NAME("admin"),												//DB에 접속할 사용자 이름을 상수로 정의
+	PASSWORD("En2i3oHKLGh9UlnbYFP1"),			//사용자의 비밀번호를 상수로 정의
+	DB_URL(			"jdbc:" + 
 					DRIVER_NAME + "://" + 
 					HOSTNAME + ":" + 
 					PORT + "/" + 
 					DB_NAME + "?user=" + 
 					USER_NAME + "&password=" + 
-					PASSWORD; 
-	
-	public static String getDriverName() {
-		return DRIVER_NAME;
-	}
-	public static String getHostname() {
-		return HOSTNAME;
-	}
-	public static String getPort() {
-		return PORT;
-	}
-	public static String getDbName() {
-		return DB_NAME;
-	}
-	public static String getUserName() {
-		return USER_NAME;
-	}
-	public static String getPassword() {
-		return PASSWORD;
-	}
-	public static String getDbUrl() {
-		return DB_URL;
+					PASSWORD);
+	DBinfo(String s)
+	{
+		
 	}
 	
 }
