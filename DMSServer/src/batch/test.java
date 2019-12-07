6ac244ae0c133220abd4a70b7119f3fc457dbc38
stimfirebat <hmlim97@naver.com>
2019-12-07 13:06:04 +0900

    명근갓;

---
+++ DMSServer/src/batch/test.java
@@ -0,0 +1,11 @@
+package batch;
+
+import java.sql.SQLException;
+
+public class test {
+	public static void main(String[] args) throws SQLException, ClassNotFoundException
+	{
+		AssignAlgorithm.batchStart();
+	}
+}
+
