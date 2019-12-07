eb9eb5bf599c89e7c68048a3f2122a8762cf7de6
dldhk97 <dldhk97@gmail.com>
2019-12-07 20:25:29 +0900

    documentManager에 DBinfo 적용

--- DMSServer/src/logic/documentManager.java
+++ DMSServer/src/logic/documentManager.java
@@ -1,11 +1,10 @@
 package logic;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import DB.DBinfo;
 import enums.Bool;
 import enums.Code1;
 import javafx.collections.FXCollections;
@@ -13,29 +12,9 @@ import javafx.collections.ObservableList;
 import models.Document;
 
 public class documentManager {
-    static final String DRIVER_NAME = "mysql";
-    static final String HOSTNAME = "wehatejava.czztgstzacsv.us-east-1.rds.amazonaws.com";
-    static final String PORT = "3306";
-    static final String DB_NAME = "Prototype";                                                    //DB이름
-    static final String USER_NAME = "admin";                                                    //DB에 접속할 사용자 이름을 상수로 정의
-    static final String PASSWORD = "En2i3oHKLGh9UlnbYFP1";                                    //사용자의 비밀번호를 상수로 정의
-    static final String DB_URL =
-            "jdbc:" +
-                    DRIVER_NAME + "://" +
-                    HOSTNAME + ":" +
-                    PORT + "/" +
-                    DB_NAME + "?user=" +
-                    USER_NAME + "&password=" +
-                    PASSWORD;
-
     public static void lookUpDocuments() throws ClassNotFoundException, SQLException    //FXCollection 몰라서 일단 띵근이가 써놓은거 따라했는데 사용법 아는사람 있으면 확인점
     {
-        Connection conn = null;
-        Statement state = null;
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
-        state = conn.createStatement();
+    	Statement state = DBinfo.connection();
 
         String callAllDocuments = "SELECT * FROM 서류";
         ResultSet documents = state.executeQuery(callAllDocuments);
@@ -51,12 +30,7 @@ public class documentManager {
     }
 
     public static void deleteDocument(String studentId, int documentType, java.util.Date submissionDate) throws ClassNotFoundException, SQLException {
-        Connection conn = null;
-        Statement state = null;
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
-        state = conn.createStatement();
+    	Statement state = DBinfo.connection();
 
         @SuppressWarnings("deprecation")
         java.sql.Date date = new java.sql.Date(submissionDate.getYear(), submissionDate.getMonth(), submissionDate.getDay());    //취소선 그어진거 잘 안쓰는 함수 쓰고있다고 경고 주는건데 대체 기능 없길래 일단 씀
@@ -71,12 +45,7 @@ public class documentManager {
     }
 
     public static void updateDocument(String studentId, int documentType, java.util.Date submissionDate, java.util.Date diagnosisDate, boolean isValid) throws ClassNotFoundException, SQLException {
-        Connection conn = null;
-        Statement state = null;
-
-        Class.forName("com.mysql.cj.jdbc.Driver");
-        conn = DriverManager.getConnection(DB_URL, USER_NAME, PASSWORD);
-        state = conn.createStatement();
+    	Statement state = DBinfo.connection();
 
         String sIsValid;
         if (isValid)
