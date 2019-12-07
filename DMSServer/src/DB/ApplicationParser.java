ad0f69cc7790449124d13c0ba688d748bef77940
sm <lunaoeu@gmail.com>
2019-12-08 01:46:31 +0900

    DB 통신 개선

--- DMSServer/src/DB/ApplicationParser.java
+++ DMSServer/src/DB/ApplicationParser.java
@@ -2,11 +2,13 @@ package DB;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.TreeSet;
 
 import enums.Grade;
 import models.Application;
 import models.Score;
+import sun.security.x509.DNSName;
 
 public class ApplicationParser {
 	public static Boolean isExist(String studentID) throws ClassNotFoundException, SQLException
@@ -46,14 +48,9 @@ public class ApplicationParser {
 	
 	public static int getNumOfLeftSeat(String dormName, int semester) throws ClassNotFoundException, SQLException
 	{
-		Statement state = DBinfo.connection();
-		Statement state2 = DBinfo.connection();
-		
 		String pureSemester = String.valueOf(semester).substring(4);
 		String getNumOfPassedAppsQuery;
 		String getCapacityQuery;
-		ResultSet passed;
-		ResultSet capacity;
 		if(pureSemester.equals("01") || pureSemester.equals("04") || pureSemester.equals("02") || pureSemester.equals("05"))
 		{
 			getNumOfPassedAppsQuery = "SELECT COUNT(*) FROM (SELECT * FROM" + DBHandler.DB_NAME + ".배정내역 WHERE 생활관명=" + dormName + " AND 학기=" + semester + ")";
@@ -64,29 +61,43 @@ public class ApplicationParser {
 			getNumOfPassedAppsQuery = "SELECT COUNT(*) FROM (SELECT * FROM " + DBHandler.DB_NAME + ".배정내역 WHERE 생활관명=" + dormName + " AND 학기=" + (semester - 1) + ")";
 			getCapacityQuery = "SELECT 수용인원 FROM 생활관정보 WHERE 생활관명=" + dormName + "AND 학기=" + (semester - 1);
 		}
-		passed = state.executeQuery(getNumOfPassedAppsQuery);
-		capacity = state2.executeQuery(getCapacityQuery);
+
+		Connection connection = DBHandler.INSTANCE.getConnetion();
+		PreparedStatement passedState = connection.prepareStatement(getNumOfPassedAppsQuery);
+		PreparedStatement capacityState = connection.prepareStatement(getCapacityQuery);
+
+		ResultSet passed = passedState.executeQuery();
+		ResultSet capacity = capacityState.executeQuery();
 		
 		capacity.next();
 		passed.next();
-		
-		return capacity.getInt("수용인원") - passed.getInt("COUNT(*)");
+
+		int result = capacity.getInt("수용인원") - passed.getInt("COUNT(*)");
+
+		passedState.close();
+		capacityState.close();
+		DBHandler.INSTANCE.returnConnection(connection);
+
+		return result;
 	}
 
 	public static TreeSet<Application> getSortedApps(String dormName, int choice, int semester) throws ClassNotFoundException, SQLException 
 	{
-		Statement state = DBinfo.connection();
-		
 		TreeSet<Application> sortedApps = new TreeSet<Application>();
 		
 		String getUnsortedAppsQuery = "SELECT * FROM " + DBHandler.DB_NAME + ".신청 WHERE 생활관명=" + dormName + " AND 지망=" + choice + "학기=201901 AND 합격여부=N";
-		ResultSet apps = state.executeQuery(getUnsortedAppsQuery);
+		Connection connection = DBHandler.INSTANCE.getConnetion();
+		PreparedStatement preparedStatement = connection.prepareStatement(getUnsortedAppsQuery);
+		ResultSet apps = preparedStatement.executeQuery();
 		
 		while(apps.next())
 		{
 			Application temp = new Application(apps.getString("학번"), apps.getString("생활관정보_생활관명"), apps.getString("생활관정보_성별"), apps.getInt("생활관정보_학기"), apps.getInt("지망"), getFinalScore(apps.getString("학번"), apps.getInt("학기")));
 			sortedApps.add(temp);
 		}
+
+		preparedStatement.close();
+		DBHandler.INSTANCE.returnConnection(connection);
 		
 		return sortedApps;
 	}
@@ -209,37 +220,47 @@ public class ApplicationParser {
 	
 	public static TreeSet<Score> getScores(String studentId, int twoSemesterBefore, int lastSemester) throws ClassNotFoundException, SQLException
 	{
-		Statement state = DBinfo.connection();
-		
 		String getScoresQuery = "SELECT 학점,등급 FROM " + DBHandler.DB_NAME + ".점수 WHERE 학번=" + studentId + "AND 학기 BETWEEN '" + twoSemesterBefore + "' AND '" + lastSemester + "'";	//직전 2학기 점수 테이블 가져오는 쿼리
-		ResultSet scores = state.executeQuery(getScoresQuery);
-		
+		Connection connection = DBHandler.INSTANCE.getConnetion();
+		PreparedStatement preparedStatement = connection.prepareStatement(getScoresQuery);
+		ResultSet scores = preparedStatement.executeQuery();
+
 		TreeSet<Score> score = new TreeSet<Score>();
 		while(scores.next())
 		{	
 			score.add(new Score(null, null, 0, scores.getInt("학점"), Grade.get(scores.getString("등급"))));
 		}
-		
+
+		preparedStatement.close();
+		DBHandler.INSTANCE.returnConnection(connection);
+
 		return score;
 	}
 	
 	public static String getZipCode(String studentId) throws ClassNotFoundException, SQLException
 	{
-		Statement state = DBinfo.connection();
-		
 		String zipCodeQuery = "SELECT 보호자우편번호 FROM " + DBHandler.DB_NAME + ".학생 WHERE 학번=" + studentId;
-		ResultSet zipCode = state.executeQuery(zipCodeQuery);
+		Connection connection = DBHandler.INSTANCE.getConnetion();
+		PreparedStatement preparedStatement = connection.prepareStatement(zipCodeQuery);
+		ResultSet zipCode = preparedStatement.executeQuery();
+
 		zipCode.next();
-		
-		return zipCode.getString("보호자우편번호");
+
+		String zipcode = zipCode.getString("보호자우편번호");
+
+		preparedStatement.close();
+		DBHandler.INSTANCE.returnConnection(connection);
+
+		return zipcode;
 	}
 
 	public static void updatePasser(Application temp) throws ClassNotFoundException, SQLException 
 	{
-		Statement state = DBinfo.connection();
-		
-		String setPassed = "UPDATE " + DBinfo.DB_NAME + ".신청 SET 합격여부=Y WHERE 학번=" + temp.getStudentId() + "지망=" + temp.getChoice() + "학기=" + temp.getSemesterCode();
-		state.executeUpdate(setPassed);
+		String setPassed = "UPDATE " + DBHandler.DB_NAME + ".신청 SET 합격여부=Y WHERE 학번=" + temp.getStudentId() + "지망=" + temp.getChoice() + "학기=" + temp.getSemesterCode();
+		Connection connection = DBHandler.INSTANCE.getConnetion();
+		PreparedStatement preparedStatement = connection.prepareStatement(setPassed);
+		ResultSet scores = preparedStatement.executeQuery();
+		preparedStatement.close();
+		DBHandler.INSTANCE.returnConnection(connection);
 	}
-	
 }
