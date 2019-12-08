d301bbcb3afbd513f684b1fbc309483375e86a52
sm <lunaoeu@gmail.com>
2019-12-08 21:10:28 +0900

    login bug fix

--- DMSServer/src/DB/AccountParser.java
+++ DMSServer/src/DB/AccountParser.java
@@ -11,7 +11,7 @@ public class AccountParser {
     public static Code2.LoginResult check(String id, String pw) throws SQLException {
         // 받아온 id pw를 db에서 조회
         Code2.LoginResult result = Code2.LoginResult.FAIL;
-        String sql = "SELECT PW, `계정 타입_코드` FROM " + DBHandler.INSTANCE.DB_NAME + ".계정 WHERE ID = " + id;
+        String sql = "SELECT PW, `계정 타입_코드` FROM " + DBHandler.INSTANCE.DB_NAME + ".계정 WHERE ID = '" + id + "'";
 
         Connection connection = DBHandler.INSTANCE.getConnetion();
         PreparedStatement state = connection.prepareStatement(sql);
