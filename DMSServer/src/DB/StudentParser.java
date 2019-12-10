6af3c8ab3966051ff12a02f56eb045d28ed6a473
예승 차 <cys7818@naver.com>
2019-12-10 17:15:23 +0900

    getAllStudent 만들었음

--- DMSServer/src/DB/StudentParser.java
+++ DMSServer/src/DB/StudentParser.java
@@ -3,8 +3,11 @@ package DB;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import enums.Gender;
+import models.Student;
 
 public class StudentParser {
     //3. 학생테이블에서 학번으로 조회하여 성별을 알아낸다.
@@ -28,5 +31,42 @@ public class StudentParser {
         	gender = Gender.get(result);
 
         return gender; //.equals("M") ? Gender.Male : Gender.Female;
+    }
+    
+    public static ArrayList<Student> getAllStudent() throws SQLException
+    {
+    	
+    	String sql = "SELECT * FROM " + DBHandler.DB_NAME + ".학생";
+    	
+    	Connection connection;
+		connection = DBHandler.INSTANCE.getConnection();
+    	
+    	PreparedStatement state;
+		state = connection.prepareStatement(sql);
+		
+		ResultSet rs;
+		
+		try 
+		{
+			rs=state.executeQuery();
+		}
+		catch (SQLException e)
+		{
+			System.out.println("sql Execute Error");
+			state.close();
+			DBHandler.INSTANCE.returnConnection(connection);
+			return null;
+		}
+		
+		ArrayList<Student> students = new ArrayList<Student>();
+		while(rs.next())
+		{
+			students.add(new Student(rs.getString("학번"), rs.getString("성명"), Gender.get(rs.getString("성별")), rs.getString("학과명"), rs.getInt("학년"), rs.getString("주민등록번호"), rs.getString("학생전화번호"), rs.getString("보호자우편번호"), rs.getString("보호자주소")));
+		}
+		
+		state.close();
+		DBHandler.INSTANCE.returnConnection(connection);
+		
+		return students;
     }
 }
